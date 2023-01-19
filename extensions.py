180b4dd5ca95584ed055480d96bd1733bd2e4ad2
Evgenii555777 <120564734+Evgenii555777@users.noreply.github.com>
2023-01-19 14:56:34 +0300

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from config import exchanges
+
+
+class APIException(Exception):
+    pass
+
+
+class Convertor:
+    @staticmethod
+    def get_price(base, sym, amount):
+        try:
+            base_key = exchanges[base.lower()]
+        except KeyError:
+            raise APIException(f"Валюта {base} не найдена!")
+
+        try:
+            sym_key = exchanges[sym.lower()]
+        except KeyError:
+            raise APIException(f"Валюта {sym} не найдена!")
+
+        if base_key == sym_key:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}!')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}!')
+
+        r = requests.get(f"https://v6.exchangerate-api.com/v6/e2355cb1d68c08c12ee51db4/latest/{base_key}")
+        resp = json.loads(r.content)
+        new_price = resp['conversion_rates'][sym_key] * float(amount)
+        new_price = round(new_price, 3)
+        message = f"Цена {amount} {base} в {sym} : {new_price}"
+        return message
+
